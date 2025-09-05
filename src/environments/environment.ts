4b25159a48d9b769d2d3aea144a7411885258b54
vikramvundyala <vikramvundyala@gmail.com>
2025-09-05 13:43:58 +0530

    Version 2 UI, first push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://13.235.31.61:8081',
+  apiUrl: 'http://3.110.218.148:8081',
   //apiUrl: 'http://localhost:8081',
   cognito: {
     userPoolId: 'ap-south-1_l57pOkQZo',
